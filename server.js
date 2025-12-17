b4839b2707a565a6011c25acef54bcb8b2fa2494
elihouchins5227-crypto <eli.houchins5227@gmail.com>
2025-12-17 00:05:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,20 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-// Serve static files from 'public' folder
+// Serve static files from public/
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route for root path
-app.get('/', (req, res) => {
+// Catch all routes and return index.html
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Example API endpoint
+// Example API endpoints
 app.get('/api/health', (req, res) => res.send('OK'));
-
-// 911 calls API
-app.get('/api/calls', (req, res) => {
-  // You can connect this to your DB later
-  res.json([{ info: 'Test call' }]);
-});
-
+app.get('/api/calls', (req, res) => res.json([{ info: 'Test 911 call' }]));
 app.post('/api/911', (req, res) => {
   const { info } = req.body;
   console.log('Received 911:', info);
